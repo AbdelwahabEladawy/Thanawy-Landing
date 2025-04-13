4a93cf349017a12142e2b64e70c6bdd2074aa6f7
abdelwhab Eladawy <abdelwahabeladawy@gmail.com>
2025-04-13 21:06:01 +0200

    fix proxy

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -1,34 +1,30 @@
-const BASE_URL = "https://backend.thanawy.com"
+const isLocal = typeof window !== "undefined" && window.location.hostname === "localhost";
+const BASE_URL = isLocal ? "/api" : "https://backend.thanawy.com";
+
 export const getSubjects = async () => {
   try {
-   
-    const response = await fetch('${BASE_URL}/subjects')
-
+    const response = await fetch(`${BASE_URL}/subjects`);
     if (!response.ok) {
-      throw new Error("")
+      throw new Error("Failed to fetch subjects");
     }
-
-    const data = await response.json()
-     return data
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error("Fetch error details:", error)
-    throw error
+    console.error("Fetch error details:", error);
+    throw error;
   }
-}
+};
+
 export const getUnits = async () => {
-    
   try {
-   
-    const response = await fetch('${BASE_URL}/units?subjects/6a345cc6-d251-423a-9712-81c89662d4e9')
-
+    const response = await fetch(`${BASE_URL}/subjects/6a345cc6-d251-423a-9712-81c89662d4e9`);
     if (!response.ok) {
-      throw new Error("")
+      throw new Error("Failed to fetch units");
     }
-
-    const data = await response.json()
-     return data
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error("Fetch error details:", error)
-    throw error
+    console.error("Fetch error details:", error);
+    throw error;
   }
-}
+};
